79a15d217dcb0d80b1c350fb12026d43e62a2389
wendel245 <48558013+wendel245@users.noreply.github.com>
2021-06-23 22:16:09 -0300

    feat(webpack-course): added a css and style loader

--- webpack-alura-1/webpack.config.js
+++ webpack-alura-1/webpack.config.js
@@ -1,17 +1,30 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // importando o plugin de compactação de html para as configurações do webpack
+// importando o plugin de compactação de html para as configurações do webpack
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 
 module.exports = {
-    entry: './app/src/js/app.js', //ponto de entrada onde será analisado o código a ser compactado
+    //ponto de entrada onde será analisado o código a ser compactado
+    entry: './app/src/js/app.js', 
     output: {
-        filename: 'bundle.js', // compacta o arquivo da entry em bundle.js
-        path: path.resolve(__dirname, 'app/dist'), // salva o arquivo compactado em uma pasta seguindo o path absoluto concatenado e resolvido pelo require
-        clean: true // apaga a pasta dist e os arquivos toda vez que for feita uma nova build
+        // compacta o arquivo da entry em bundle.js
+        filename: 'bundle.js', 
+        // salva o arquivo compactado em uma pasta seguindo o path absoluto concatenado e resolvido pelo require
+        path: path.resolve(__dirname, 'app/dist'), 
+        // apaga a pasta dist e os arquivos toda vez que for feita uma nova build
+        clean: true
     },
+    // utiliza o modulo css-loader para compilar arquivos que terminam com .css importados no app.js no bundle.js
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader','css-loader'] }
+        ]
+    },
+    // instanciando a classe onde o plugin foi importado para gerar o html com o bundle.js referenciado
     plugins: [new HtmlWebpackPlugin({
         template: './app/src/app.html',
         filename: 'app.html',
-        hash: true // gerar um hash no arquivo bundle.js usado para não conflitar o js quando o mesmo fica armazenado no cache
-    })] // instanciando a classe onde o plugin foi importado para gerar o html com o bundle.js referenciado
+        // gerar um hash no arquivo bundle.js usado para não conflitar o js quando o mesmo fica armazenado no cache
+        hash: true 
+    })] 
 
 };
