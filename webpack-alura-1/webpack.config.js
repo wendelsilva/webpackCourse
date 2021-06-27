d50f4ef246cba8f8c27b61a2c69c0e2ebc787c0f
wendel245 <48558013+wendel245@users.noreply.github.com>
2021-06-27 15:43:42 -0300

    feat(webpack-course): config a css comp/mininmizer and webpack default server and lazy loading

--- webpack-alura-1/webpack.config.js
+++ webpack-alura-1/webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 // importando o plugin de compactação de html para as configurações do webpack
 const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+// importando plugin para criar um arquivo de main.css fora do bundle.js
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// plugin para minimizar os arquivos css
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+// plugin para concatenar os arquivos
+const webpack = require('webpack');
 
 module.exports = {
     //ponto de entrada onde será analisado o código a ser compactado
@@ -16,15 +22,36 @@ module.exports = {
     // utiliza o modulo css-loader para compilar arquivos que terminam com .css importados no app.js no bundle.js
     module: {
         rules: [
-            { test: /\.css$/, use: ['style-loader','css-loader'] }
+            { test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader,'css-loader'] }
+        ]
+    },
+    // aplica as minificações que nos definimos e remove as minificações padrões do webpack
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerWebpackPlugin(),
+            // mantem o comportamento de minificar os arquivos padrão do webpack pois ao adicionar uma tag optimize ele para de aplicar essas minificações por padrão
+            '...'
         ]
     },
     // instanciando a classe onde o plugin foi importado para gerar o html com o bundle.js referenciado
     plugins: [new HtmlWebpackPlugin({
-        template: './app/src/app.html',
-        filename: 'app.html',
-        // gerar um hash no arquivo bundle.js usado para não conflitar o js quando o mesmo fica armazenado no cache
-        hash: true 
-    })] 
-
+            template: './app/src/index.html',
+            filename: 'index.html',
+            // gerar um hash no arquivo bundle.js usado para não conflitar o js quando o mesmo fica armazenado no cache
+            hash: true 
+        }),
+        // plugin cria um arquivo de css separado do bundle.js na pasta dist
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        // concatena os arquivos
+        new webpack.optimize.ModuleConcatenationPlugin()
+    ],
+    // utilizando o servidor de desenvolvimento do webpack
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 3000
+    }
 };
